0bb67b7710251c09d0bc377c552952b04d0e4e7c
megan <mtpott23@gmail.com>
2022-06-26 12:19:38 -0400

    css stylings for about page fixed, photo added

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import photo from '../../assets/me.png';
 
 function About() {
     return (
+        <div className="container">
         <div className='about-div'>
             <h3 className='about'>About the developer:</h3>
-                <p className='about-pg'>Megan is originally from Durham, North Carolina, and graduated from Davidson College in 2019 with a degree in Art History. After making her way back to Durham by way of Boston, MA, she enrolled at UNC Charlotte's full stack development bootcamp. She has previously worked as a barista at a local coffeeshop, and enjoyed the bustle of a busy day, as there was always something to occupy the mind. Before working as a barista, she served as an intern in the Development department at the Peabody Essex Museum in Salem, MA, and was sad to see the internship cut short due to COVID-19. In her free time, she enjoys yoga, reading, and playing The Legend of Zelda (she has already completed Breath of the Wild twice, and is anxiously awaiting part two).</p>
+                <p className='about-pg'>Megan is originally from Durham, North Carolina, and graduated from Davidson College in 2019 with a degree in Art History. After making her way back to Durham by way of Boston, MA, she enrolled at UNC Charlotte's full stack development bootcamp. She has previously worked as a barista at a local coffeeshop, and enjoyed the bustle of a busy day, as there was always something to occupy the mind. Before working as a barista, she served as an intern in the Development department at the Peabody Essex Museum in Salem, MA, and was sad to see the internship cut short due to COVID-19. Throughout her bootcamp experience, she has most enjoyed working with Node, Express, and SQL.</p>
 
                 <p className='about-pg'>Contact Megan here:</p>
-                <br></br>
                 <a href="mailto:mtpott23@gmail.com">mtpott23@gmail.com</a>
                 <p>(919)906-8451</p>
-
-        <div>need to add a photo!</div>
-        </div>
+            </div>
+            <div className="photo">
+                <img alt="me" src={photo}></img>
+            </div>
+    </div>
     )
 }
 
