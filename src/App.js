dee939885118e99cef53c7a3b9eb3d97bf4e0aff
megan <mtpott23@gmail.com>
2022-09-07 09:47:57 -0400

    change title to name

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    document.title = 'Megan Pottenger'
+  }, [])
   return (
     <div>
      <Header />
